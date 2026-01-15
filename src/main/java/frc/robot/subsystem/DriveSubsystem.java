e038072d95e39b89dd55002b2bf9d69ce1e14d11
Momentum Robotics <momentumrobotics@gmail.com>
2026-01-14 17:13:18 -0800

    Implement Drive Logic

--- src/main/java/frc/robot/subsystem/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystem/DriveSubsystem.java
@@ -1,9 +1,46 @@
 package frc.robot.subsystem;
 
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.io.File;
+import java.util.function.Supplier;
+import swervelib.SwerveDrive;
+import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 
-public class DriveSubsystem extends SubsystemBase{
+public class DriveSubsystem extends SubsystemBase {
+
+    private final File directory = new File(Filesystem.getDeployDirectory(), "swerve");
+    private final SwerveDrive swerveDrive;
+
     public DriveSubsystem() {
+        SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+        try {
+            swerveDrive = new SwerveParser(directory).createSwerveDrive(5); // change to a moPref later
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public void driveFieldOriented(ChassisSpeeds velocity) {
+        swerveDrive.driveFieldOriented(velocity);
+    }
+
+    public Command resetFieldOrientedFwd() {
+        return runOnce(() -> swerveDrive.setGyro(Rotation3d.kZero));
+    }
+
+    public Command driveFieldOriented(Supplier<ChassisSpeeds> velocity) {
+        return run(() -> {
+            swerveDrive.driveFieldOriented(velocity.get());
+        });
+    }
+
+    public SwerveDrive getSwerveDrive() {
+        return swerveDrive;
     }
 }
