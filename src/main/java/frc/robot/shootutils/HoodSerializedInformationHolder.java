ff9cebcd865e377f55f861a010d38f63ed2c4ed2
Jude Rasmussen <jude.a.rasmussen@gmail.com>
2026-01-16 18:27:44 -0800

    Update logic for extreme cases

--- src/main/java/frc/robot/shootutils/HoodSerializedInformationHolder.java
+++ src/main/java/frc/robot/shootutils/HoodSerializedInformationHolder.java
@@ -47,13 +47,19 @@ public class HoodSerializedInformationHolder {
         });
         double lowerBound = distances.get(0);
         double upperBound = distances.get(distances.size() - 1);
-        int i = 0;
-        for (double d : distances) {
-            if (distance > d) {
-                lowerBound = d;
-                upperBound = distances.get(i + 1);
+        if (distance < lowerBound) {
+            upperBound = distances.get(1);
+        } else if (distance > upperBound) {
+            lowerBound = distances.get(distances.size() - 2);
+        } else {
+            int i = 0;
+            for (double d : distances) {
+                if (distance > d) {
+                    lowerBound = d;
+                    upperBound = distances.get(i + 1);
+                }
+                i++;
             }
-            i++;
         }
         double m = (angles.get(upperBound) - angles.get(lowerBound)) / (upperBound - lowerBound);
         return angles.get(lowerBound) + (m * (distance - lowerBound));
@@ -66,13 +72,19 @@ public class HoodSerializedInformationHolder {
         });
         double lowerBound = distances.get(0);
         double upperBound = distances.get(distances.size() - 1);
-        int i = 0;
-        for (double d : distances) {
-            if (distance > d) {
-                lowerBound = d;
-                upperBound = distances.get(i + 1);
+        if (distance < lowerBound) {
+            upperBound = distances.get(1);
+        } else if (distance > upperBound) {
+            lowerBound = distances.get(distances.size() - 2);
+        } else {
+            int i = 0;
+            for (double d : distances) {
+                if (distance > d) {
+                    lowerBound = d;
+                    upperBound = distances.get(i + 1);
+                }
+                i++;
             }
-            i++;
         }
         double m = (flywheelSpeeds.get(upperBound) - flywheelSpeeds.get(lowerBound)) / (upperBound - lowerBound);
         return flywheelSpeeds.get(lowerBound) + (m * (distance - lowerBound));
