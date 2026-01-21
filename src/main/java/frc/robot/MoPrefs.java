3658d1ff9746eb3d92c37af21a1fb8261087c00d
Jordan Powers <jordanpowers1227@gmail.com>
2026-01-20 21:04:52 -0800

    Rename to MoPrefs

--- src/main/java/frc/robot/MoPrefs.java
+++ src/main/java/frc/robot/MoPrefs.java
@@ -2,9 +2,9 @@ package frc.robot;
 
 import edu.wpi.first.units.Units;
 import frc.robot.molib.prefs.LinearVelocityUnitPref;
-import frc.robot.molib.prefs.MoPrefs;
+import frc.robot.molib.prefs.MoPrefsBase;
 
-public class Prefs extends MoPrefs {
+public class MoPrefs extends MoPrefsBase {
     public static LinearVelocityUnitPref examplePreference =
             metersPerSecPref("DUMMY PREF", Units.MetersPerSecond.of(5));
 }
