849ffad6419bfe01d8125d06e94d37f627407dc8
Jordan Powers <jordanpowers1227@gmail.com>
2026-01-20 21:20:59 -0800

    Add moprefs for swerve max speed

--- src/main/java/frc/robot/subsystem/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystem/DriveSubsystem.java
@@ -2,9 +2,11 @@ package frc.robot.subsystem;
 
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.MoPrefs;
 import java.io.File;
 import java.util.function.Supplier;
 import swervelib.SwerveDrive;
@@ -20,10 +22,22 @@ public class DriveSubsystem extends SubsystemBase {
     public DriveSubsystem() {
         SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
         try {
-            swerveDrive = new SwerveParser(directory).createSwerveDrive(5); // change to a moPref later
+            swerveDrive = new SwerveParser(directory)
+                    .createSwerveDrive(MoPrefs.swerveMaxPossibleSpeed.get().in(Units.MetersPerSecond));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+
+        MoPrefs.swerveMaxPossibleSpeed.subscribe(speed -> swerveDrive.setMaximumAttainableSpeeds(
+                speed.in(Units.MetersPerSecond),
+                MoPrefs.swerveMaxPossibleSpin.get().in(Units.RadiansPerSecond)));
+        MoPrefs.swerveMaxPossibleSpin.subscribe(spin -> swerveDrive.setMaximumAttainableSpeeds(
+                MoPrefs.swerveMaxPossibleSpeed.get().in(Units.MetersPerSecond), spin.in(Units.RadiansPerSecond)));
+        MoPrefs.swerveMaxAllowedSpeed.subscribe(speed -> swerveDrive.setMaximumAllowableSpeeds(
+                speed.in(Units.MetersPerSecond),
+                MoPrefs.swerveMaxAllowedSpin.get().in(Units.RadiansPerSecond)));
+        MoPrefs.swerveMaxAllowedSpin.subscribe(spin -> swerveDrive.setMaximumAllowableSpeeds(
+                MoPrefs.swerveMaxAllowedSpeed.get().in(Units.MetersPerSecond), spin.in(Units.RadiansPerSecond)));
     }
 
     public void driveFieldOriented(ChassisSpeeds velocity) {
