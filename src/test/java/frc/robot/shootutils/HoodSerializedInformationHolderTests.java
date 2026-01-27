e16a1c936f55ace10708432ebeda707af5e3b5b2
Jude Rasmussen <jude.a.rasmussen@gmail.com>
2026-01-26 16:45:54 -0800

    Change tests and create test config file

--- src/test/java/frc/robot/shootutils/HoodSerializedInformationHolderTests.java
+++ src/test/java/frc/robot/shootutils/HoodSerializedInformationHolderTests.java
@@ -10,63 +10,55 @@ public class HoodSerializedInformationHolderTests {
 
     @Test
     public void testAngleInterpolation() {
-        HoodSerializedInformationHolder holder = new HoodSerializedInformationHolder(List.of(
-                new HoodSerializedInformationHolder.Entry(1f, 1500f, 5f),
-                new HoodSerializedInformationHolder.Entry(2f, 1600f, 6f)));
+        HoodSerializedInformationHolder holder = HoodSerializedInformationHolder.create();
 
-        assertEquals(5.0, holder.getAngle(1.0), TOLERANCE);
-        assertEquals(6.0, holder.getAngle(2.0), TOLERANCE);
+        assertEquals(1.0, holder.getAngle(1.0), TOLERANCE);
+        assertEquals(2.0, holder.getAngle(2.0), TOLERANCE);
 
-        assertEquals(5.5, holder.getAngle(1.5), TOLERANCE);
-        assertEquals(5.25, holder.getAngle(1.25), TOLERANCE);
+        assertEquals(1.5, holder.getAngle(1.5), TOLERANCE);
+        assertEquals(1.25, holder.getAngle(1.25), TOLERANCE);
 
-        assertEquals(4, holder.getAngle(0), TOLERANCE);
-        assertEquals(4.5, holder.getAngle(0.5), TOLERANCE);
+        assertEquals(0.0, holder.getAngle(0), TOLERANCE);
+        assertEquals(0.5, holder.getAngle(0.5), TOLERANCE);
 
-        assertEquals(6.5, holder.getAngle(2.5), TOLERANCE);
+        assertEquals(5.0, holder.getAngle(3.5), TOLERANCE);
         assertEquals(8.0, holder.getAngle(4), TOLERANCE);
     }
 
     @Test
     public void testFlywheelSpeedInterpolation() {
-        HoodSerializedInformationHolder holder = new HoodSerializedInformationHolder(List.of(
-                new HoodSerializedInformationHolder.Entry(5f, 1500f, 5f),
-                new HoodSerializedInformationHolder.Entry(10f, 1600f, 6f)));
+        HoodSerializedInformationHolder holder = HoodSerializedInformationHolder.create();
 
-        assertEquals(1500.0, holder.getFlywheelSpeed(5.0), TOLERANCE);
-        assertEquals(1600.0, holder.getFlywheelSpeed(10.0), TOLERANCE);
+        assertEquals(1.0, holder.getFlywheelSpeed(1.0), TOLERANCE);
+        assertEquals(2.0, holder.getFlywheelSpeed(2.0), TOLERANCE);
 
-        assertEquals(1520.0, holder.getFlywheelSpeed(6.0), TOLERANCE);
-        assertEquals(1550.0, holder.getFlywheelSpeed(7.5), TOLERANCE);
+        assertEquals(1.5, holder.getFlywheelSpeed(1.5), TOLERANCE);
+        assertEquals(1.25, holder.getFlywheelSpeed(1.25), TOLERANCE);
 
-        assertEquals(1480.0, holder.getFlywheelSpeed(4.0), TOLERANCE);
-        assertEquals(1620.0, holder.getFlywheelSpeed(11.0), TOLERANCE);
+        assertEquals(0.0, holder.getFlywheelSpeed(0), TOLERANCE);
+        assertEquals(0.5, holder.getFlywheelSpeed(0.5), TOLERANCE);
+
+        assertEquals(5.0, holder.getFlywheelSpeed(3.5), TOLERANCE);
+        assertEquals(8.0, holder.getFlywheelSpeed(4), TOLERANCE);
     }
 
     @Test
     public void testAngleInterpolationNonLinear() {
-        HoodSerializedInformationHolder holder = new HoodSerializedInformationHolder(List.of(
-                new HoodSerializedInformationHolder.Entry(1f, 1500f, 5f),
-                new HoodSerializedInformationHolder.Entry(2f, 1600f, 6f),
-                new HoodSerializedInformationHolder.Entry(3.5f, 1700f, 9f)));
+        HoodSerializedInformationHolder holder = HoodSerializedInformationHolder.create();
 
-        assertEquals(4.5, holder.getAngle(0.5), TOLERANCE);
-        assertEquals(5.5, holder.getAngle(1.5), TOLERANCE);
-        assertEquals(7.0, holder.getAngle(2.5), TOLERANCE);
-        assertEquals(10.0, holder.getAngle(4.0), TOLERANCE);
+        assertEquals(0.5, holder.getAngle(0.5), TOLERANCE);
+        assertEquals(1.5, holder.getAngle(1.5), TOLERANCE);
+        assertEquals(3, holder.getAngle(2.5), TOLERANCE);
+        assertEquals(6, holder.getAngle(4.0), TOLERANCE);
     }
 
     @Test
     public void testFlywheelSpeedInterpolationNonLinear() {
-        HoodSerializedInformationHolder holder = new HoodSerializedInformationHolder(List.of(
-                new HoodSerializedInformationHolder.Entry(5f, 1500f, 5f),
-                new HoodSerializedInformationHolder.Entry(10f, 1600f, 6f),
-                new HoodSerializedInformationHolder.Entry(20f, 1700f, 7f)));
+        HoodSerializedInformationHolder holder = HoodSerializedInformationHolder.create();
 
-        assertEquals(1480.0, holder.getFlywheelSpeed(4.0), TOLERANCE);
-        assertEquals(1550.0, holder.getFlywheelSpeed(7.5), TOLERANCE);
-        assertEquals(1610.0, holder.getFlywheelSpeed(11.0), TOLERANCE);
-        assertEquals(1650.0, holder.getFlywheelSpeed(15.0), TOLERANCE);
-        assertEquals(1710.0, holder.getFlywheelSpeed(21.0), TOLERANCE);
+        assertEquals(0.5, holder.getFlywheelSpeed(0.5), TOLERANCE);
+        assertEquals(1.5, holder.getFlywheelSpeed(1.5), TOLERANCE);
+        assertEquals(3, holder.getFlywheelSpeed(2.5), TOLERANCE);
+        assertEquals(6, holder.getFlywheelSpeed(4.0), TOLERANCE);
     }
 }
