25556c1d5cacd2adbb7decd2e0e7b238486f8e10
Momentum Robotics <momentumrobotics@gmail.com>
2026-02-03 18:39:03 -0800

    Intake Ports

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,6 +3,10 @@ package frc.robot;
 public class Constants {
     public static final HIDPort DRIVE_CONTORLLER_PORT = new HIDPort(0);
 
+    //Intake Ports
+    public static final CANAddress INTAKE_ROLLER_PORT = new CANAddress(0); //edit this with the actual robot
+    public static final CANAddress INTAKE_WRIST_PORT = new CANAddress(0);
+
     public static record CANAddress(int address) {}
 
     public static record HIDPort(int hidport) {}
