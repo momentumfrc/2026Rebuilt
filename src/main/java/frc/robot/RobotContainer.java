9a7a765e5ab70ece2f38b7e89fb0f6e29b6df23e
Jordan Powers <jordanpowers1227@gmail.com>
2026-02-08 15:20:01 -0800

    Odometry (#3)
    * Add LimelightHelpers.java
    
    * Add PositioningSubsystem
    
    * Add geometry logic to TurretSubsystem
    
    * Add support for second limelight to PositioningSubsystem
    
    * Instantiate timer in PositioningSubsystem
    
    * Fix null handling when adding vision measurements
    
    * Update limelight names

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,10 +10,15 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.input.ControllerInput;
 import frc.robot.input.MoInput;
 import frc.robot.subsystem.DriveSubsystem;
+import frc.robot.subsystem.PositioningSubsystem;
+import frc.robot.subsystem.TurretSubsystem;
 import swervelib.SwerveInputStream;
 
 public class RobotContainer {
     private final DriveSubsystem driveSubsystem = new DriveSubsystem();
+    private final TurretSubsystem turretSubsystem = new TurretSubsystem();
+    private final PositioningSubsystem visionSubsystem =
+            new PositioningSubsystem(driveSubsystem.getSwerveDrive(), turretSubsystem::getTurretLimelightPose);
 
     private Trigger resetFieldOrientedFwd;
 
