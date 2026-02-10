55f71662bfab361f35fccd10e7a474afeeca1a96
Bryan <bryanhh.lb@gmail.com>
2026-02-09 19:17:58 -0800

    Added Operator Controller

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,9 +2,10 @@ package frc.robot;
 
 public class Constants {
     public static final HIDPort DRIVE_CONTORLLER_PORT = new HIDPort(0);
+    public static final HIDPort OPERATOR_CONTROLLER_PORT = new HIDPort(1);
 
-    //Intake Ports
-    public static final CANAddress INTAKE_ROLLER_PORT = new CANAddress(0); //edit this with the actual robot
+    // Intake Ports
+    public static final CANAddress INTAKE_ROLLER_PORT = new CANAddress(0); // edit this with the actual robot
     public static final CANAddress INTAKE_WRIST_PORT = new CANAddress(0);
 
     public static record CANAddress(int address) {}
