36f19cf589c3165b37ce75359308320fc7e3bc2c
Bryan <bryanhh.lb@gmail.com>
2026-02-09 19:16:13 -0800

    Made Intake Current Preferences

--- src/main/java/frc/robot/MoPrefs.java
+++ src/main/java/frc/robot/MoPrefs.java
@@ -1,11 +1,13 @@
 package frc.robot;
 
+import edu.wpi.first.units.CurrentUnit;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.VoltageUnit;
 import frc.robot.molib.prefs.AngularVelocityUnitPref;
 import frc.robot.molib.prefs.LinearVelocityUnitPref;
 import frc.robot.molib.prefs.MoPrefsBase;
 import frc.robot.molib.prefs.Pref;
+import frc.robot.molib.prefs.TimeUnitPref;
 import frc.robot.molib.prefs.UnitPref;
 
 public class MoPrefs extends MoPrefsBase {
@@ -24,6 +26,11 @@ public class MoPrefs extends MoPrefsBase {
     public static final UnitPref<VoltageUnit> intakeWristVoltage = voltsPref("Intake Wrist Power", Units.Volts.of(5));
     public static final UnitPref<VoltageUnit> intakeWristHoldVoltage =
             voltsPref("Intake Wrist Hold Power", Units.Volts.of(1));
+
+    public static final UnitPref<CurrentUnit> intakeWristSmartCurrentLimit =
+            ampsPref("Intake Wrist Smart Current Limit", Units.Amps.of(20));
+
+    public static final TimeUnitPref intakeCurrentWristTime = secondsPref("Intake Current Wrist Time", Units.Seconds.one());
 
     public static final Pref<Double> inputDeadband = unitlessDoublePref("Input DeadBand", 0.05);
 }
