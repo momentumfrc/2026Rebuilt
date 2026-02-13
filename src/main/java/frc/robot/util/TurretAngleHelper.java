337f36d31c89b9c393defcfffab4452e5659a4aa
Jordan Powers <jordanpowers1227@gmail.com>
2026-02-12 22:13:22 -0800

    Add TurretAngleHelper

---
+++ src/main/java/frc/robot/util/TurretAngleHelper.java
@@ -0,0 +1,42 @@
+package frc.robot.util;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public class TurretAngleHelper {
+    private static final double FLOAT_DELTA = 1e-9;
+
+    private final Rotation2d minAngle;
+    private final Rotation2d maxAngle;
+
+    public TurretAngleHelper(Rotation2d minAngle, Rotation2d maxAngle) {
+        if (maxAngle.getRadians() <= minAngle.getRadians()) {
+            throw new IllegalArgumentException("max angle must be greater than min angle");
+        }
+
+        this.minAngle = minAngle;
+        this.maxAngle = maxAngle;
+    }
+
+    /**
+     * Limits values to within [minAngle, maxAngle]. Returns null if a value is out of range.
+     */
+    public Rotation2d turretAngleModulus(Rotation2d angle) {
+        double rads = angle.getRadians();
+        double min = minAngle.getRadians();
+        double max = maxAngle.getRadians();
+
+        if (rads < min) {
+            rads = MathUtil.inputModulus(rads, min, min + 2 * Math.PI);
+        }
+        if (rads > max) {
+            rads = MathUtil.inputModulus(rads, max - 2 * Math.PI, max);
+        }
+
+        if (min - rads > FLOAT_DELTA || rads - max > FLOAT_DELTA) {
+            return null;
+        }
+
+        return Rotation2d.fromRadians(rads);
+    }
+}
