d838b6d0855b86448224624ee780dc0c7c2fe329
Jordan Powers <jordanpowers1227@gmail.com>
2026-02-18 21:44:54 -0800

    Revert "Move shooter data to network tables (#10)"
    This reverts commit a723f3782f8324a5c474109ef7040986b1221116.

--- src/main/java/frc/robot/shootutils/ShootMath.java
+++ src/main/java/frc/robot/shootutils/ShootMath.java
@@ -1,94 +1,76 @@
 package frc.robot.shootutils;
 
-import edu.wpi.first.units.Units;
-import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.units.measure.AngularVelocity;
-import edu.wpi.first.units.measure.Distance;
-import edu.wpi.first.units.measure.MutAngle;
-import edu.wpi.first.units.measure.MutAngularVelocity;
-import edu.wpi.first.units.measure.MutDistance;
-import java.util.List;
-import java.util.function.Supplier;
+import edu.wpi.first.math.geometry.Translation2d;
 
 public class ShootMath {
-    public static class Solution {
-        private final MutDistance distance = Units.Meters.mutable(0);
-        private final MutAngle hoodAngle = Units.Radians.mutable(0);
-        private final MutAngularVelocity flywheelSpeed = Units.RPM.mutable(0);
 
-        public Distance distance() {
-            return distance;
-        }
+    public static final double G = 9.81;
 
-        public Angle hoodAngle() {
-            return hoodAngle;
-        }
-
-        public AngularVelocity flywheelSpeed() {
-            return flywheelSpeed;
-        }
+    /**
+     * Calculates the desired heading of the hood.
+     * @param x the current x position of the robot
+     * @param y the current y position of the robot
+     * @param targetPosX the x position of the target (hub) in the same coordinate system as the robot position
+     * @param targetPosY the y position of the target (hub) in the same coordinate system as the robot position
+     * @return the heading the hood should aim at, measured from the x-axis, in the current coordinate system
+     */
+    public static double hoodAimAngle(double x, double y, double targetPosX, double targetPosY) {
+        return Math.atan2(targetPosY - y, targetPosX - x);
     }
 
-    private static void interpolate(
-            List<ShooterEmpiricalDataStore.Datum> data,
-            Distance distance,
-            MutAngle hoodAngle,
-            MutAngularVelocity flywheelSpeed) {
-        assert data.size() >= 2;
-
-        int lowerBound = 0;
-        int upperBound = data.size() - 1;
-        if (distance.lt(data.get(lowerBound).distance())) {
-            upperBound = 1;
-        } else if (distance.gt(data.get(upperBound).distance())) {
-            lowerBound = data.size() - 2;
-        } else {
-            for (int i = 1; i < data.size(); i++) {
-                if (distance.lt(data.get(i).distance())) {
-                    lowerBound = i - 1;
-                    upperBound = i;
-                    break;
-                }
-            }
-        }
-        var lowerDatum = data.get(lowerBound);
-        var upperDatum = data.get(upperBound);
-
-        assert lowerDatum.distance().lt(upperDatum.distance());
-
-        double run =
-                upperDatum.distance().in(Units.Meters) - lowerDatum.distance().in(Units.Meters);
-
-        double angleM = (upperDatum.hoodAngle().in(Units.Radians)
-                        - lowerDatum.hoodAngle().in(Units.Radians))
-                / run;
-
-        double speedM = (upperDatum.flywheelSpeed().in(Units.RPM)
-                        - lowerDatum.flywheelSpeed().in(Units.RPM))
-                / run;
-
-        double distDiff = distance.in(Units.Meters) - lowerDatum.distance().in(Units.Meters);
-
-        hoodAngle.mut_replace(lowerDatum.hoodAngle().in(Units.Radians) + (angleM * distDiff), Units.Radians);
-
-        flywheelSpeed.mut_replace(lowerDatum.flywheelSpeed().in(Units.RPM) + (speedM * distDiff), Units.RPM);
+    /**
+     * Calculates the desired heading of the hood.
+     * @param robot a {@link Translation2d} representing the position of the robot
+     * @param target a {@link Translation2d} representing the position of the robot
+     * @return the heading the hood should aim at, measured from the x-axis, in the current coordinate system
+     */
+    public static double hoodAimAngle(Translation2d robot, Translation2d target) {
+        return hoodAimAngle(robot.getX(), robot.getY(), target.getX(), target.getY());
     }
 
-    static Supplier<List<ShooterEmpiricalDataStore.Datum>> dataSupplier =
-            () -> ShooterEmpiricalDataStore.getInstance().getSortedData();
+    /**
+     * Calculates the needed hood angle to make it into the hub. This uses an empirical calculation.
+     * @param x the current x position of the robot
+     * @param y the current y position of the robot
+     * @param targetPosX the x position of the target (hub) in the same coordinate system as the robot position
+     * @param targetPosY the y position of the target (hub) in the same coordinate system as the robot position
+     * @return the needed hood angle to make it into the hub
+     */
+    public static double hoodAngle(double x, double y, double targetPosX, double targetPosY) {
+        HoodSerializedInformationHolder holder = HoodSerializedInformationHolder.create();
+        return holder.getAngle(Math.pow(Math.pow(x - targetPosX, 2) + Math.pow(y - targetPosY, 2), 0.5));
+    }
 
-    private static Solution solution = new Solution();
+    /**
+     * Calculates the needed hood angle to make it into the hub. This uses an empirical calculation.
+     * @param robot a {@link Translation2d} representing the position of the robot
+     * @param target a {@link Translation2d} representing the position of the robot
+     * @return the needed hood angle to make it into the hub
+     */
+    public static double hoodAngle(Translation2d robot, Translation2d target) {
+        return hoodAngle(robot.getX(), robot.getY(), target.getX(), target.getY());
+    }
 
-    public static Solution solve(Distance distance) {
-        if (solution.distance().isNear(distance, 1e-6)) {
-            return solution;
-        }
+    /**
+     * Calculates the needed flywheel speed to make it into the hub. This uses an empirical calculation.
+     * @param x the current x position of the robot
+     * @param y the current y position of the robot
+     * @param targetPosX the x position of the target (hub) in the same coordinate system as the robot position
+     * @param targetPosY the y position of the target (hub) in the same coordinate system as the robot position
+     * @return the needed flywheel speed to make it into the hub
+     */
+    public static double flywheelSpeed(double x, double y, double targetPosX, double targetPosY) {
+        HoodSerializedInformationHolder holder = HoodSerializedInformationHolder.create();
+        return holder.getFlywheelSpeed(Math.pow(Math.pow(x - targetPosX, 2) + Math.pow(y - targetPosY, 2), 0.5));
+    }
 
-        var data = dataSupplier.get();
-
-        solution.distance.mut_replace(distance);
-        interpolate(data, distance, solution.hoodAngle, solution.flywheelSpeed);
-
-        return solution;
+    /**
+     * Calculates the needed flywheel speed to make it into the hub. This uses an empirical calculation.
+     * @param robot a {@link Translation2d} representing the position of the robot
+     * @param target a {@link Translation2d} representing the position of the robot
+     * @return the needed flywheel speed to make it into the hub
+     */
+    public static double flywheelSpeed(Translation2d robot, Translation2d target) {
+        return flywheelSpeed(robot.getX(), robot.getY(), target.getX(), target.getY());
     }
 }
