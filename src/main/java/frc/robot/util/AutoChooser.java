ec45fd5d9d004e222dd37273d2b057914dfe2250
Momentum Robotics <momentumrobotics@gmail.com>
2026-02-18 18:15:57 -0800

    Made AutoChoices Sendable Chooser

--- src/main/java/frc/robot/util/AutoChooser.java
+++ src/main/java/frc/robot/util/AutoChooser.java
@@ -1,18 +1,25 @@
 package frc.robot.util;
 
-import com.pathplanner.lib.path.PathPlannerPath;
-
 import edu.wpi.first.networktables.BooleanEntry;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.RobotPositioning;
 import frc.robot.subsystem.DriveSubsystem;
 
 public class AutoChooser {
+    private enum AutoChoices {
+        LEAVE,
+        SHOOT
+    }
+
     private final RobotPositioning robotPositioning;
     private final DriveSubsystem driveSubsystem;
 
     private final BooleanEntry enableAutoSwitch;
+
+    private SendableChooser<AutoChoices> autoChoicesChooser = enumToChooser(AutoChoices.class);
 
     public AutoChooser(RobotPositioning robotPositioning, DriveSubsystem driveSubsystem) {
         this.robotPositioning = robotPositioning;
@@ -20,6 +27,9 @@ public class AutoChooser {
 
         var autoTable = NTHelpers.getTable("Auto");
         enableAutoSwitch = NTHelpers.getBooleanEntry(autoTable, "Run Auto?", true);
+        ;
+
+        SmartDashboard.putData("Auto Choices", autoChoicesChooser);
     }
 
     public Command getAutoChooserCommand() {
@@ -27,5 +37,20 @@ public class AutoChooser {
             return Commands.print("Auto Disabled");
         }
         return null;
+    }
+
+    public <T extends Enum<?>> SendableChooser<T> enumToChooser(Class<T> toConvert) {
+        return enumToChooser(toConvert, toConvert.getEnumConstants()[0]);
+    }
+
+    public <T extends Enum<?>> SendableChooser<T> enumToChooser(Class<T> toConvert, T defaultValue) {
+        var chooser = new SendableChooser<T>();
+        chooser.setDefaultOption(defaultValue.name(), defaultValue);
+        for (T entry : toConvert.getEnumConstants()) {
+            if (entry != defaultValue) {
+                chooser.addOption(entry.name(), entry);
+            }
+        }
+        return chooser;
     }
 }
