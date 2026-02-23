6239f6ceb353e7af40e25125653150cba2bcf9c2
Jordan Powers <jordanpowers1227@gmail.com>
2026-02-22 19:44:20 -0800

    Publish sysid choosers to sysid table

--- src/main/java/frc/robot/util/SysIdUtil.java
+++ src/main/java/frc/robot/util/SysIdUtil.java
@@ -8,7 +8,6 @@ import edu.wpi.first.networktables.NetworkTableEvent;
 import edu.wpi.first.units.AngularAccelerationUnit;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.Subsystem;
@@ -66,8 +65,8 @@ public class SysIdUtil {
             routineCache.clear();
         });
 
-        SmartDashboard.putData("SysId Mode", modeChooser);
-        SmartDashboard.putData("SysId Mechanism", mechanismChooser);
+        NTHelpers.publishSendable(table, "SysId Mode", modeChooser);
+        NTHelpers.publishSendable(table, "SysId Mechanism", mechanismChooser);
     }
 
     private SysIdRoutine.Config getSysIdConfig() {
