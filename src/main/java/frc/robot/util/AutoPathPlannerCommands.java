aad7c31d57b699bcf550f4006e07d5f8d04b73f0
Bryan <bryanhh.lb@gmail.com>
2026-02-22 21:44:25 -0800

    Fixed AutoPathPlannerCommands

--- src/main/java/frc/robot/util/AutoPathPlannerCommands.java
+++ src/main/java/frc/robot/util/AutoPathPlannerCommands.java
@@ -12,56 +12,49 @@ import frc.robot.RobotPositioning;
 import frc.robot.subsystem.DriveSubsystem;
 
 public class AutoPathPlannerCommands {
-    private final RobotPositioning robotPositioning;
-    private final DriveSubsystem driveSubsystem;
 
     public AutoPathPlannerCommands(RobotPositioning robotPositioning, DriveSubsystem driveSubsystem) {
-        this.robotPositioning = robotPositioning;
-        this.driveSubsystem = driveSubsystem;
-
         RobotConfig config;
         try {
             config = RobotConfig.fromGUISettings();
+
+            // Configure AutoBuilder last
+            AutoBuilder.configure(
+                    robotPositioning::getRobotPose, // Robot pose supplier
+                    robotPositioning
+                            ::resetOdometry, // Method to reset odometry (will be called if your auto has a starting
+                    // pose)
+                    robotPositioning::getRobotVelocity, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+                    (speeds, feedforwards) -> driveSubsystem.driveFieldOriented(speeds),
+                    new PPHolonomicDriveController(
+                            new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+                            new PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
+                            ),
+                    config, // The robot configuration
+                    () -> {
+                        // Boolean supplier that controls when the path will be mirrored for the red alliance
+                        // This will flip the path being followed to the red side of the field.
+                        // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+                        var alliance = DriverStation.getAlliance();
+                        if (alliance.isPresent()) {
+                            return alliance.get() == DriverStation.Alliance.Red;
+                        }
+                        return false;
+                    },
+                    driveSubsystem // Reference to this subsystem to set requirements
+                    );
         } catch (Exception e) {
-            // Handle exception as needed
             e.printStackTrace();
         }
-
-        // Configure AutoBuilder last
-        AutoBuilder.configure(
-                robotPositioning::getRobotPose, // Robot pose supplier
-                this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
-                driveSubsystem::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-                (speeds, feedforwards) ->
-                        (speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also
-                // optionally outputs individual module feedforwards
-                new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for
-                        // holonomic drive trains
-                        new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
-                        new PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
-                        ),
-                config, // The robot configuration
-                () -> {
-                    // Boolean supplier that controls when the path will be mirrored for the red alliance
-                    // This will flip the path being followed to the red side of the field.
-                    // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
-
-                    var alliance = DriverStation.getAlliance();
-                    if (alliance.isPresent()) {
-                        return alliance.get() == DriverStation.Alliance.Red;
-                    }
-                    return false;
-                },
-                this // Reference to this subsystem to set requirements
-                );
     }
 
-    public Command getFollowPathCommand() {
+    public static Command getFollowPathCommand(
+            DriveSubsystem driveSubsystem, RobotPositioning robotPositioning, String pathName) {
         try {
             // Load the path you want to follow using its name in the GUI
-            PathPlannerPath path = PathPlannerPath.fromPathFile("Example Path");
+            PathPlannerPath path = PathPlannerPath.fromPathFile(pathName);
 
-            // Create a path following command using AutoBuilder. This will also trigger event markers.
             return AutoBuilder.followPath(path);
         } catch (Exception e) {
             DriverStation.reportError("Big oops: " + e.getMessage(), e.getStackTrace());
