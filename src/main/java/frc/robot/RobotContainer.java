690bdc961286c91d114b2af9a21a070ff51f0465
Jude Rasmussen <80215501+TheElementGuy@users.noreply.github.com>
2026-02-25 15:43:35 -0800

    Add lock pose (#16)
    * Add lock pose
    
    * Log calculated hood angle and flywheel speed
    
    * Revert "Log calculated hood angle and flywheel speed"
    
    This reverts commit 06d48f5f842b83931cceca2b45a3d77831bf422f.
    
    * Add lock pose command (no trigger)
    
    * Now triggered!
    
    * Apply spotless
    
    * Fix a nit
    
    * Apply spotless BY HAND

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -90,6 +90,8 @@ public class RobotContainer {
 
     private Trigger runSysIdTrigger;
 
+    private Trigger lockTrigger;
+
     public RobotContainer() {
         configureBindings();
         setDefaultCommands();
@@ -134,6 +136,8 @@ public class RobotContainer {
 
         runSysIdTrigger = new Trigger(() -> getInput().getRunSysId());
 
+        lockTrigger = new Trigger(() -> getInput().getLockRequest());
+
         // Drive Trigger Bindings
         resetFieldOrientedFwd.onTrue(driveSubsystem.resetFieldOrientedFwd());
 
@@ -147,6 +151,8 @@ public class RobotContainer {
         zeroHoodTrigger.and(RobotModeTriggers.disabled().negate()).onTrue(zeroHoodCommand);
 
         runSysIdTrigger.whileTrue(sysId.getSysIdCommand());
+
+        lockTrigger.whileTrue(driveSubsystem.lockPose());
 
         RobotModeTriggers.test().whileTrue(turretTestCommand);
         RobotModeTriggers.test().and(zeroHoodTrigger.negate()).whileTrue(hoodTestCommand);
--- src/main/java/frc/robot/subsystem/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystem/DriveSubsystem.java
@@ -119,4 +119,10 @@ public class DriveSubsystem extends SubsystemBase {
     public Command getTeleopDriveCommand(Supplier<MoInput> inputSupplier) {
         return driveFieldOriented(setupDriveModes(inputSupplier));
     }
+
+    public Command lockPose() {
+        return run(() -> {
+            swerveDrive.lockPose();
+        });
+    }
 }
