5254d2f761fad16bce590fc40257d930f92cc5ab
Momentum Robotics <momentumrobotics@gmail.com>
2026-03-17 18:37:48 -0700

    Added Single Controller Class

---
+++ src/main/java/frc/robot/input/SingleControllerInput.java
@@ -0,0 +1,87 @@
+package frc.robot.input;
+
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.Constants;
+
+public class SingleControllerInput implements MoInput{
+    private final XboxController driveController = new XboxController(Constants.DRIVE_CONTORLLER_PORT.hidport());
+
+    public XboxController getDriveController() {
+        return driveController;
+    }
+
+    @Override
+    public double getDriveMoveXRequest() {
+        return -1 * driveController.getLeftY();
+    }
+
+    @Override
+    public double getDriveMoveYRequest() {
+        return -1 * driveController.getLeftX();
+    }
+
+    @Override
+    public double getDriveTurnRequest() {
+        return -1 * driveController.getRightX();
+    }
+
+    @Override
+    public double getDriveHeadingXRequest() {
+        return driveController.getRightY();
+    }
+
+    @Override
+    public double getDriveHeadingYRequest() {
+        return driveController.getRightX();
+    }
+
+    @Override
+    public boolean getReZeroGyro() {
+        return driveController.getBackButton();
+    }
+
+    @Override
+    public boolean getRunIntake() {
+        return driveController.getRightBumperButton();
+    }
+
+    @Override
+    public boolean getAgitate() {
+        return driveController.getLeftBumperButton();
+    }
+
+    @Override
+    public boolean getClearShooter() {
+        return driveController.getBButton();
+    }
+
+    @Override
+    public boolean getExtendIntake() {
+        return driveController.getLeftTriggerAxis() > 0;
+    }
+
+    @Override
+    public boolean getRetractIntake() {
+        return driveController.getRightTriggerAxis() > 0;
+    }
+
+    @Override
+    public boolean getShootRequest() {
+        return driveController.getAButton();
+    }
+
+    @Override
+    public boolean getReverseIndexerRequest() {
+        return driveController.getYButton();
+    }
+
+    @Override
+    public boolean getRunSysId() {
+        return driveController.getStartButton();
+    }
+
+    @Override
+    public boolean getLockRequest() {
+        return driveController.getXButton();
+    }
+}
