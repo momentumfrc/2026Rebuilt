044ee0b426ec0ed7f8fce2357b6e676903f2ac0f
Momentum Robotics <momentumrobotics@gmail.com>
2026-03-17 17:51:32 -0700

    RobotContainer setup

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -93,7 +93,7 @@ public class RobotContainer {
     private final Command intakeRollerDefaultCommand = RollerCommands.idleIntakeRollerCommand(intakeRollerSubsystem);
     private final Command intakeWristDefaultCommand = WristCommands.intakeWristDefaultCommand(intakeWristSubsystem);
 
-    private final Command ledCommand = new LEDCommand(leds, robotPositioning, turret);
+    private final Command ledCommand = new LEDCommand(leds, robotPositioning, turret, intakeRollerSubsystem, shootCommand);
 
     // **** TRIGGERS ****
     private Trigger resetFieldOrientedFwd;
