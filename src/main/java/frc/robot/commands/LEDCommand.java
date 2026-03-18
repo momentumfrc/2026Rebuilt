768e92f9a9fd7f12368384600ba64943dae0c353
Momentum Robotics <momentumrobotics@gmail.com>
2026-03-17 17:48:38 -0700

    LED Commands for Intake and Shoot Command

--- src/main/java/frc/robot/commands/LEDCommand.java
+++ src/main/java/frc/robot/commands/LEDCommand.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotPositioning;
+import frc.robot.subsystem.IntakeRollerSubsystem;
 import frc.robot.subsystem.LEDSubsystem;
 import frc.robot.subsystem.TurretSubsystem;
 
@@ -12,6 +13,8 @@ public class LEDCommand extends Command {
     private final LEDSubsystem leds;
     private final RobotPositioning positioning;
     private final TurretSubsystem turret;
+    private final IntakeRollerSubsystem rollerSubsystem;
+    private final ShootCommand shootCommand;
 
     private static final LEDPattern inRangePattern =
             LEDPattern.solid(Color.kGreen).blink(Units.Seconds.of(0.1));
@@ -19,12 +22,18 @@ public class LEDCommand extends Command {
     private static final LEDPattern outOfRangePattern =
             LEDPattern.solid(Color.kRed).blink(Units.Seconds.of(0.5));
 
+    private static final LEDPattern runIntakePattern = LEDPattern.solid(Color.kBlue).blink(Units.Seconds.of(0.5));
+
+    private static final LEDPattern shootingPattern = LEDPattern.solid(Color.kOrange).blink(Units.Seconds.of(0.5));
+
     private static final LEDPattern noInitialPositionPattern = LEDPattern.solid(Color.kOrange);
 
-    public LEDCommand(LEDSubsystem leds, RobotPositioning positioning, TurretSubsystem turret) {
+    public LEDCommand(LEDSubsystem leds, RobotPositioning positioning, TurretSubsystem turret, IntakeRollerSubsystem rollerSubsystem, ShootCommand shootCommand) {
         this.leds = leds;
         this.positioning = positioning;
         this.turret = turret;
+        this.rollerSubsystem = rollerSubsystem;
+        this.shootCommand = shootCommand;
 
         addRequirements(leds);
     }
@@ -36,11 +45,20 @@ public class LEDCommand extends Command {
             return;
         }
 
+        if (rollerSubsystem.runningRollers()) {
+            leds.applyPattern(runIntakePattern);
+        }
+
+        if (shootCommand.currentlyShooting()) {
+            leds.applyPattern(shootingPattern);
+        }
+
         if (turret.targetInRange()) {
             leds.applyPattern(inRangePattern);
         } else {
             leds.applyPattern(outOfRangePattern);
         }
+
     }
 
     @Override
