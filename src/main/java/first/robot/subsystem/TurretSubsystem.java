8614d32d3474bbf6a6acaaab77c028d568be324e
Jordan Powers <jordanpowers1227@gmail.com>
2026-09-08 18:54:08 -0700

    Use Angle in TurretAngleHelper
    # Conflicts:
    #	src/test/java/frc/robot/utils/TurretAngleHelperTests.java

--- src/main/java/first/robot/subsystem/TurretSubsystem.java
+++ src/main/java/first/robot/subsystem/TurretSubsystem.java
@@ -96,7 +96,7 @@ public class TurretSubsystem extends SubsystemBase {
     private final MoTalonFxProfilePID<AngleUnit, AngularVelocityUnit> turretAbsolutePid;
     private final PIDController turretRelativePid;
 
-    private Rotation2d lastOORAngle = null;
+    private Angle lastOORAngle = null;
     private final LinearFilter turretOORVelocityFilter =
             LinearFilter.movingAverage((int) (0.1 / Constants.LOOP_PERIOD));
 
@@ -190,9 +190,7 @@ public class TurretSubsystem extends SubsystemBase {
                 MoPrefs.turretMinSoftLimit,
                 MoPrefs.turretMaxSoftLimit,
                 (min, max) -> {
-                    angleHelper = new TurretAngleHelper(
-                            Rotation2d.fromRadians(min.in(Units.Radians)),
-                            Rotation2d.fromRadians(max.in(Units.Radians)));
+                    angleHelper = new TurretAngleHelper((Angle) min, (Angle) max);
                 },
                 true);
 
@@ -311,13 +309,13 @@ public class TurretSubsystem extends SubsystemBase {
         alignAbsolute(setpoint.goalAngle(), setpoint.goalVelocity());
     }
 
-    private double calculateOutOfRangeVelocity(Rotation2d angle) {
+    private double calculateOutOfRangeVelocity(Angle angle) {
         if (lastOORAngle == null) {
             turretOORVelocityFilter.reset();
             lastOORAngle = angle;
         }
         double velocity =
-                turretOORVelocityFilter.calculate(angle.minus(lastOORAngle).getDegrees() / Constants.LOOP_PERIOD);
+                turretOORVelocityFilter.calculate(angle.minus(lastOORAngle).in(Units.Degrees) / Constants.LOOP_PERIOD);
         lastOORAngle = angle;
         return velocity;
     }
@@ -343,10 +341,10 @@ public class TurretSubsystem extends SubsystemBase {
 
         State goalState;
         if (result.inRange()) {
-            goalState = new State(result.angle().getDegrees(), goalVelocity.in(Units.DegreesPerSecond));
+            goalState = new State(result.angle().in(Units.Degrees), goalVelocity.in(Units.DegreesPerSecond));
             lastOORAngle = null;
         } else {
-            goalState = new State(result.angle().getDegrees(), calculateOutOfRangeVelocity(result.angle()));
+            goalState = new State(result.angle().in(Units.Degrees), calculateOutOfRangeVelocity(result.angle()));
         }
 
         if (Math.abs(absoluteSetpoint.position - turretEncoder.getPosition().in(Units.Degrees))
@@ -423,7 +421,7 @@ public class TurretSubsystem extends SubsystemBase {
                         targetInRange.set(result.inRange());
 
                         this.turretAbsolutePid.setReference(
-                                Units.Radians.of(result.angle().getRadians()), Units.RadiansPerSecond.zero());
+                                result.angle(), Units.RadiansPerSecond.zero());
                     }
                 })
                 .withName("TurretTestCommand");
